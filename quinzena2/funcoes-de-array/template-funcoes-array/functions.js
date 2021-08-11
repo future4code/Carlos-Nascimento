d22daf07284a2d90473438cfa5e47508c1f04d3b
Vinicius <cvinicius@outlook.com>
2021-08-10 23:04:29 -0300

    Commit inicial

---
+++ quinzena2/funcoes-de-array/template-funcoes-array/functions.js
@@ -0,0 +1,10 @@
+const verificarPar = (numero) => {
+
+   if (numero % 2 === 0) {
+      const resultado = numero / 2
+   }
+}
+
+const imprimirMensagem = (valor) => {
+   console.log("O resultado da sua conta é:", valor)
+}
